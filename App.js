9d03978025bdb041b11e01b35f5f7547cfb8116f
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-01-03 17:14:06 +0500

    [QA] Added API to get weather

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import React from "react";
+import {Alert} from "react-native";
+import Loading from "./components/Loading";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "d7b504927f176b42e4e1f8be9a3861ea";
+export default class extends React.Component {
+    state = {
+        isLoading: true
+    }
+
+    getWeatherFromAPI = async (latitude, longitude) => {
+        const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        console.log('api',data)
+    }
+
+    getLocation = async () => {
+        try {
+            await Location.requestForegroundPermissionsAsync()
+            const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+            await this.getWeatherFromAPI(latitude, longitude)
+            this.setState({isLoading: false})
+        } catch (e) {
+            Alert.alert("Не могу определить местоположение", "Очень грустно :(")
+        }
+    }
+
+    componentDidMount() {
+        this.getLocation()
+    }
+
+    render() {
+        const {isLoading} = this.state;
+
+        return (
+            isLoading ? <Loading/> : null
+        );
+    }
+    ;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
